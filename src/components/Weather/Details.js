3675fb558d3e4b8c1b0dfc4b26fe385d51a95373
Rohit Dhiman <rohitdhiman937@gmail.com>
2023-12-28 18:03:03 +0530

    add sun image

--- src/components/Weather/Details.js
+++ src/components/Weather/Details.js
@@ -1,13 +1,20 @@
-
+import SunLogo from '../../assets/images/sun.png'
 const WeatherDetails = () => {
     return (
-        <div className=" w-10/12">
-            <div className="flex flex-col gap-2">
-                <span className="text-5xl font-bold">Madrid</span>
-                <span className="text-[#a5bbd8]">Chance of rain</span>
-                <span className="text-5xl mt-4 font-bold">31&deg;C</span>
+        <div className='flex flex-col w-10/12 gap-8'>
+            <div className="flex justify-between items-center">
+                <div className="flex flex-col gap-2">
+                    <span className="text-5xl font-bold">Madrid</span>
+                    <span className="text-[#a5bbd8]">Chance of rain</span>
+                    <span className="text-5xl mt-10 font-bold">31&deg;C</span>
+                </div>
+                <div>
+                    <img src={SunLogo} width={180} height={150} alt='sunlogo' />
+                </div>
+            </div>
+            <div className='flex flex-col p-5 bg-[#EAECEF] rounded-md'>
 
-
+                <span className='font-semibold text-sm'>TODAY'S FORECAST</span>
             </div>
         </div>
     )
