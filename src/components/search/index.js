629a658421a89617b595fcd90c0362ffb9c60597
Rohit Dhiman <rohitdhiman937@gmail.com>
2023-12-28 17:49:33 +0530

    made weather panel

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchBar = () => {
     return (
-        <div className='flex p-1  bg-gray-100 border-2 rounded-md'>
+        <div className='flex p-1 bg-gray-100 border-2 rounded-md'>
             <input placeholder='Search for cities' className='w-full p-1 rounded-md bg-gray-100 focus:outline-none' />
         </div>
     )
