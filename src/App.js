ea75ce90671c947a32219ca731430315ec882be3
Rohit Dhiman <rohitdhiman937@gmail.com>
2023-12-28 17:49:18 +0530

    made weather detail panel

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import SideBar from './components/Sidebar';
+import WeatherDetails from './components/Weather/Details';
 import SearchBar from './components/search';
 
 function App() {
   return (
     <div className='flex w-full border-black'>
+
       <div className='h-screen p-8'>
         <SideBar />
       </div>
-      <div className='w-3/5 b ml-5 p-8'>
-        <SearchBar />
+      <div className='flex flex-col w-3/5 ml-5'>
+        <div className='p-8'>
+          <SearchBar />
+        </div>
+        <div className='flex justify-center'>
+          <WeatherDetails />
+        </div>
       </div>
     </div>
   );
