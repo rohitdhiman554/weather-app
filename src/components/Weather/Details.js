69f1186a69f10f95b73126ed990f3633648f27aa
Rohit Dhiman <rohitdhiman937@gmail.com>
2023-12-28 17:49:49 +0530

    remove space

--- src/components/Weather/Details.js
+++ src/components/Weather/Details.js
@@ -5,7 +5,7 @@ const WeatherDetails = () => {
             <div className="flex flex-col gap-2">
                 <span className="text-5xl font-bold">Madrid</span>
                 <span className="text-[#a5bbd8]">Chance of rain</span>
-                <span className="text-5xl mt-4 font-bold">31&deg; C</span>
+                <span className="text-5xl mt-4 font-bold">31&deg;C</span>
 
 
             </div>
