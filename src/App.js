5173e08634fdfdfec856db8bf1d1a923c95243e1
Rohit Dhiman <rohitdhiman937@gmail.com>
2023-12-30 23:07:56 +0530

    removed propertie

--- src/App.js
+++ src/App.js
@@ -73,13 +73,13 @@ function App() {
   };
 
   return (
-    <div className="flex w-full justify-between lg:justify-normal mx-auto h-full gap-4">
+    <div className="flex  lg:justify-normal mx-auto h-full gap-4">
       <div className="fixed w-full border-b bg-white">
-        <div className="px-8 mx-auto">
+        <div className="max-w-7xl px-8 mx-auto">
           <SearchBar setSearch={setSearch} isLoading={isLoading} />
         </div>
       </div>
-      <div className="flex flex-col md:flex-row px-5 mx-auto h-full gap-4 mt-24">
+      <div className="flex flex-col md:flex-row max-w-7xl px-5 mx-auto h-full gap-4 mt-24">
         <div className={clsx(!isError ? "md:overflow-y-auto scroll-container overflow-x-hidden md:h-[85vh]" : null)}>
           {isError ? (
             <ErrorPage errorMessage={ERROR_MESSAGE} />
